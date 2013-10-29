2c8bed545e24ad67188e1964c8edba0ae2825f93
alexoner <onerhao@gmail.com>
2013-10-29 22:21:29 +0800

    modified README.md

--- httpd.c
+++ httpd.c
@@ -20,6 +20,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 #include <errno.h>
 #include <unistd.h>
 #include <dirent.h>
@@ -31,21 +33,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define LISTENQ					5
-#define DEFAULT_PORT			9117
-#define ROOT_DIR				""
-#define MAX_NAME_VALUE			255
-#define MAXLINE					1024
-#define MAX_METHOD_L			127
-#define MAX_URL_L				255
-#define MAX_PATH_L				511
-#define MAX_VERSION_L			15
-#define MAX_CGI_DIR_N			2
-#define MAX_MIME_L				255
-#define SERVER					"Server:HTTP/0.1 C/99"
-#define CONTENT_LENGTH_S		"Content-Length:"
+#include "httpd.h"
+#include "http_epoll.h"
+#include "http_urldecode.h"
 
-#define DEBUG					1
 
 typedef enum
 {
@@ -85,95 +76,10 @@ typedef enum
 } request;
 */
 
-struct request
-{
-    char method[MAX_METHOD_L];
-    char url[MAX_URL_L];
-    char *query_string;
-};
+extern http_epoll_t http_epoll;
 
 char root_dir[MAX_PATH_L];
 char *cgi_dir[2]= {"cgi-bin","htdocs"};
-
-//initialize the server
-int startserver(int *listenfd,int *port);
-
-//Handle request until shutdown
-int serve_forever(int listenfd);
-
-//Handle a request then exit
-void handle_request(int connfd);
-
-//read a character from the stdio
-int readc(int fd,void *buf,size_t count);
-
-//read a line from the stdio
-int readline(int fd,char *buf,size_t maxlen);
-
-//accept a request from the socket
-int accept_request(int connfd,char **request);
-
-void verify_request(const char *request);
-
-//process the request
-int process_request(int connfd,const char *request);
-
-/*Sends and logs a complete error reply to the client.
- * The numeric code specifies the HTTP error code
- */
-void send_error(int connfd,int error_code,const char* message);
-
-//Writes a specific HTTP header to the output stream
-void send_headers(int connfd,const char *mimetype,size_t size);
-
-//Sends a blank line, indicating the end of the HTTP headers in the response
-void end_headers(int connfd);
-
-//send the content of a file
-int send_content(int connfd,const char *mime,const char *path);
-
-//send binary file
-int send_binary(int connfd,const char *path);
-
-//send text file
-int send_text(int connfd,const char *path);
-
-//directory listing
-int dirlist(int connfd,const char *url,const char *path);
-
-//execute it as cgi script
-int serve_dynamic(int connfd, const char* path,
-                  const char* method, const char* query_string);
-
-//serve it as a file
-int serve_static(int connfd,const char *path);
-
-//read and discard data in the socket
-int clear_socket(int connfd);
-
-//read and discards headers
-int read_headers(int connfd,char *buf,size_t count);
-
-//get mime file types
-char *get_mime_t(char *type,const char *path);
-
-//print error and exit
-void error_die(const char *s);
-
-//send error response headers.
-void send_error(int connfd,int code,const char *message);
-
-//unimplemented method
-void unimplemented(int connfd);
-
-//file not found
-void not_found(int connfd,const char *path);
-
-//file cannot be executed
-void cannot_execute(int connfd);
-
-//bad request
-void bad_request(int connfd);
 
 
 /**********************************************************************/
@@ -314,11 +220,13 @@ int process_request(int connfd,const char *request)
     int i,cgi=0;
     if(!request)
         return -1;
-    if((ns=sscanf(request,"%s %s %s",method,url,version))!=3)
+    if((ns=sscanf(request,"%s %s %s",method,buf,version))!=3)
     {
         bad_request(connfd);
     }
+	urldecode(buf,url);
     printf("%s",request);
+	//printf("%s\n",url);
     //process for different methods
     if(!strcasecmp(method,"GET"))
     {
@@ -335,7 +243,8 @@ int process_request(int connfd,const char *request)
         }
         else
         {
-            sscanf(url,"%s",buf);
+            //sscanf(url,"%s",buf);
+            strcpy(buf,url);
             sprintf(path,"%s%s",root_dir,buf);
             for(i=0; i<MAX_CGI_DIR_N; i++)
             {
@@ -364,7 +273,9 @@ int process_request(int connfd,const char *request)
     {
         //POST method
         cgi=1;
-        sscanf(url,"%s",buf);
+        //sscanf(url,"%s",buf);
+        strcpy(url,buf);
+		printf("url: %s\n",buf);
         sprintf(path,"%s%s",root_dir,buf);
 
         //for debug
@@ -382,7 +293,7 @@ int process_request(int connfd,const char *request)
     {
         perror("stat ");
         getcwd(buf,sizeof(buf));
-        printf("cwd is %s\n",buf);
+        printf("file is %s\n",path);
         not_found(connfd,path);
     }
     else
@@ -432,7 +343,7 @@ void end_headers(int connfd)
     //a blank line seperate headers from the content
 }
 
-int  send_content(int connfd,const char *mime,const char* path)
+int send_content(int connfd,const char *mime,const char* path)
 {
 	char str[MAX_MIME_L];
 	if(sscanf(mime,"%[^/]",str)<=0)
